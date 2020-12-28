4b5a80741ab4180a9916d0ddfcb6c5e9f2be0081
Nuthasith <nuthasith1997@gmail.com>
2020-12-28 21:01:37 +0700

    . . .

---
+++ apt_nav/src/vel_filter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+
+class velFilter:
+	def __init__(self):
+		rospy.init_node('vel_filter')
+		print "Node 'vel_filter' has initialized."
+
+		rospy.Subscriber('/cmd_vel', Twist, self.vel_filters)
+
+		self.pub_filtered_vel = rospy.Publisher('/mavros/setpoint_velocity/cmd_vel_unstamped', Twist, queue_size=5)
+
+		self.filtered_vel = Twist()
+		self.filtered_vel.linear.x = 0
+		self.filtered_vel.linear.y = 0
+		self.filtered_vel.linear.z = 0
+		self.filtered_vel.angular.x = 0
+		self.filtered_vel.angular.y = 0
+		self.filtered_vel.angular.z = 0
+
+	def vel_filters(self, msg):
+		self.filtered_vel.linear.y = msg.linear.x
+		self.filtered_vel.angular.z = msg.angular.z
+		self.pub_filtered_vel.publish(self.filtered_vel)
+
+if __name__ == '__main__':
+	velFilter()
+	rospy.spin()
